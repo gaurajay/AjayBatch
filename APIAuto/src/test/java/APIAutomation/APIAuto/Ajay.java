e1ea62f83d848c9c345b5e0623b463809a01ca2c
o <o@DESKTOP-II32ER0>
2020-05-05 20:23:52 +0530

    Pushing second time

---
+++ APIAuto/src/test/java/APIAutomation/APIAuto/Ajay.java
@@ -0,0 +1,11 @@
+package APIAutomation.APIAuto;
+
+public class Ajay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HAdifh;odsf");
+
+	}
+
+}
